5944427ecfd6c2954e2ddc76830ef46bcb79524d
Michael Martin <martinmike2@gmail.com>
2015-08-28 12:27:01 +0000

    initial commit

---
+++ src/Fuelingbrands/GoogleApiClient/GoogleApiClientServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Fuelingbrands\GoogleApiClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class GoogleApiClientServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
