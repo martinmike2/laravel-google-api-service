92380ce235eb7b21eb9ee3a1c1c85efd67cef08a
Michael Martin <martinmike2@gmail.com>
2015-08-31 19:35:39 +0000

    added constants to Drive class for roles

--- src/Fuelingbrands/GoogleApiClient/Drive/Drive.php
+++ src/Fuelingbrands/GoogleApiClient/Drive/Drive.php
@@ -8,6 +8,16 @@ abstract class Drive
     use GoogleApiTrait;
     protected $drive;
 
+    const DRIVE = 'https://www.googleapis.com/auth/drive';
+    const DRIVE_FILE = 'https://www.googleapis.com/auth/drive.file';
+    const DRIVE_APPS_READONLY = 'https://www.googleapis.com/auth/drive.apps.readonly';
+    const DRIVE_READONLY = 'https://www.googleapis.com/auth/drive.readonly';
+    const DRIVE_METADATA_READONLY = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+    const DRIVE_METADATA = 'https://www.googleapis.com/auth/drive.metadata';
+    const DRIVE_INSTALL = 'https://www.googleapis.com/auth/drive.install';
+    const DRIVE_APPFOLDER = 'https://www.googleapis.com/auth/drive.appfolder';
+    const DRIVE_SCRIPTS = 'https://www.googleapis.com/auth/drive.scripts';
+
     public function __construct($email, $private_key, $scopes, $impersonated_email)
     {
         $this->client = GoogleClient::getInstance($email, $private_key, $scopes, $impersonated_email);
